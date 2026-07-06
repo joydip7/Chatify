e18fffb36691c5e625ab4c3b4724c15568f0319a
Joydip <joydip.insight@gmail.com>
2026-07-06 21:27:56 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.routes.js";
+
+const app = express();
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} `);
+});
