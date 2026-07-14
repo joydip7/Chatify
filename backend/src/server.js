4c241aded754c33df9e4a99a1dd237075057b9e0
Joydip <joydip.insight@gmail.com>
2026-07-15 03:17:04 +0530

    signup page added

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,8 @@ import path from "path";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 import cookieParser from "cookie-parser";
+import cors from "cors"
+
 
 const app = express();
 
@@ -13,6 +15,7 @@ const app = express();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json())  //req.body
+app.use(cors({origin:ENV.CLIENT_URL, credentials: true}));
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div className>
+      Login Page
+    </div>
+  )
+}
+
+export default LoginPage
