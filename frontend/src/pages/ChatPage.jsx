6e03d57d2411183417e8d07b061e0be3bb5937b2
Joydip <joydip.insight@gmail.com>
2026-07-15 23:17:37 +0530

    LoginLogout Implemented

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useAuthStore } from '../store/useAuthStore'
 
 const ChatPage = () => {
+  const {logout} = useAuthStore();
   return (
-    <div>
+    <div className='z-10'>
       CHAT PAGE
+      <button onClick={logout}>logout</button>
     </div>
   )
 }
